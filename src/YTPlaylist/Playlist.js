2a6b334d1d3a206beab9661040c83dbc84009867
deeppaz <deeppaz@hotmail.com>
2020-07-04 15:18:59 +0300

    update

--- src/YTPlaylist/Playlist.js
+++ src/YTPlaylist/Playlist.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react'
-import YouTube from 'react-youtube';
 import './Playlist.css'
 
 class Playlist extends Component {
-    _onReady(event) {
-        // access to player in all event handlers via event.target
-        event.target.pauseVideo();
-    }
     render() {
-        const opts = {
-            height: '90',
-            width: '640',
-            playerVars: {
-                // https://developers.google.com/youtube/player_parameters
-                autoplay: 1,
-            },
-        };
         return (
             <div>
-                <iframe class="iframe" width="100%" height="465" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/users/346386776&amp;auto_play=true&amp;buying=false&amp;liking=false&amp;download=false&amp;sharing=false&amp;show_artwork=false&amp;show_comments=false&amp;show_playcount=true&amp;show_user=true&amp;hide_related=false&amp;visual=true&amp;start_track=0&amp;callback=true"></iframe>
+                {/* <iframe title="serenesmusicplayer" className="PlaylistContainer" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/bentrok/growing-a-little-metropolis&amp;auto_play=true&amp;color=%23000000&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true" loading="lazy">
+                </iframe> */}
+                <iframe title="serenesmusicplayer" className="PlaylistContainer" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/users/334535459&amp;auto_play=true&amp;buying=false&amp;liking=false&amp;download=false&amp;sharing=false&amp;color=%23000000&amp;show_artwork=false&amp;show_comments=false&amp;show_playcount=false&amp;show_user=false&amp;hide_related=false&amp;visual=false&amp;start_track=0&amp;callback=true"></iframe>
             </div >
         )
     }
---
+++ src/data/Music.js
@@ -0,0 +1,18 @@
+export const songs = [
+    {
+      url: 'path/to/mp3',
+      cover: 'path/to/jpeg',
+      artist: {
+        name: 'Metallica',
+        song: 'Fuel'
+      }
+    },
+    {
+      url: 'path/to/your/mp3',
+      artist: {
+        name: 'X Japan',
+        song: 'Art of Life'
+      }
+    }
+  ];
+  
