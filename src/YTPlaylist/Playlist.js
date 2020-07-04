a9e829f2e4b25f7bff5e8d8b938b69b888134b10
deeppaz <deeppaz@hotmail.com>
2020-07-05 00:21:52 +0300

    update

--- src/YTPlaylist/Playlist.js
+++ src/YTPlaylist/Playlist.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
-import './Playlist.css'
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+import { Rnd } from "react-rnd";
+
+import './Playlist.css';
 
 class Playlist extends Component {
     render() {
         return (
             <div>
-                {/* <iframe title="serenesmusicplayer" className="PlaylistContainer" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/bentrok/growing-a-little-metropolis&amp;auto_play=true&amp;color=%23000000&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true" loading="lazy">
-                </iframe> */}
-                <iframe title="serenesmusicplayer" className="PlaylistContainer" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/users/334535459&amp;auto_play=true&amp;buying=false&amp;liking=false&amp;download=false&amp;sharing=false&amp;color=%23000000&amp;show_artwork=false&amp;show_comments=false&amp;show_playcount=false&amp;show_user=false&amp;hide_related=false&amp;visual=false&amp;start_track=0&amp;callback=true"></iframe>
+                {/* <iframe title="serenesmusicplayer" className="PlaylistContainer" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/users/334535459&amp;auto_play=true&amp;buying=false&amp;liking=false&amp;download=false&amp;sharing=false&amp;color=%23000000&amp;show_artwork=false&amp;show_comments=false&amp;show_playcount=false&amp;show_user=false&amp;hide_related=false&amp;visual=false&amp;start_track=0&amp;callback=true"></iframe> */}
+                <Rnd
+                    default={{
+                        x: 0,
+                        y: 0,
+                        width: 600,
+                        height: 500
+                    }}
+                >
+                    <div className='player-wrapper'>
+                        <ReactPlayer
+                            playing="true"
+                            muted="true"
+                            playbackRate="3"
+                            config={{
+                                youtube: {
+                                    playerVars: { showinfo: 0 }
+                                }
+                            }}
+                            className="react-player"
+                            url={[
+                                'https://www.youtube.com/watch?v=oUFJJNQGwhk',
+                                'https://www.youtube.com/watch?v=jNgP6d9HraI'
+                            ]}
+                        />
+                    </div>
+                </Rnd>
             </div >
         )
     }
