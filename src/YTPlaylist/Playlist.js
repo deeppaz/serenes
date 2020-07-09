af7bb381a072333d5a2319847d10d66106ea03dd
deeppaz <deeppaz@hotmail.com>
2020-07-09 15:40:04 +0300

    update

--- src/YTPlaylist/Playlist.js
+++ src/YTPlaylist/Playlist.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { Media, Player, controls } from 'react-media-player'
-import YTPlaylists from '../data/playlist.json';	
+import YTPlaylists from '../data/playlist.json';
 import './Playlist.css';
-
 
 class Playlist extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            YTPlaylist: Math.floor(Math.random() * YTPlaylists.length),	
+            YTPlaylist: Math.floor(Math.random() * YTPlaylists.length),
             curTime: new Date().toLocaleString(),
+            active: false
         };
     }
-    
+
     componentWillMount() {
         setInterval(function () {
             this.setState({
@@ -20,6 +20,8 @@ class Playlist extends Component {
             })
         }.bind(this), 1000);
     }
+
+    toggleSoundIcon = () => this.setState((currentState) => ({active: !currentState.active}));
 
     render() {
         const {
@@ -34,21 +36,21 @@ class Playlist extends Component {
             <div>
 
                 <Media>
-                        <>
-                            <Player autoPlay src={YTPlaylists[this.state.YTPlaylist].musics} className="media-player" />
-                            <div className="media-controls">
-                                <h1 className="saat">{this.state.curTime}</h1>
-                         
-                                <PlayPause className="playButton" />
-                                <CurrentTime className="currentTime" />
-                                <SeekBar className="sliderTime" />
-                                <Duration className="duration" />
-                                <div className="volumeContainer">
-                                    <MuteUnmute className="soundToggle" />
-                                    <Volume className="volumeSlider" />
-                                </div>
+                    <>
+                        <Player autoPlay src={YTPlaylists[this.state.YTPlaylist].musics} className="media-player" />
+                        <div className="media-controls">
+                            <h1 className="saat">{this.state.curTime}</h1>
+
+                            <PlayPause className="playButton" />
+                            <CurrentTime className="currentTime" />
+                            <SeekBar className="sliderTime" />
+                            <Duration className="duration" />
+                            <div className="volumeContainer">
+                                <MuteUnmute  className={this.state.active ? 'soundToggle' : 'soundToggle2'} onClick={this.toggleSoundIcon} />
+                                <Volume className="volumeSlider" />
                             </div>
-                        </>
+                        </div>
+                    </>
                 </Media>
             </div >
         )
