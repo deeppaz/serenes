d6b1df861c2bb081121a3920df0a75b907359baf
deeppaz <deeppaz@hotmail.com>
2020-07-29 22:47:17 +0300

    update

--- src/data/Playlist.js
+++ src/data/Playlist.js
@@ -1,5 +1,7 @@
 export const YTPlaylists = [
     'https://www.youtube.com/watch?v=tFD-61WBN1Q',
     'https://www.youtube.com/watch?v=feb2yAbR8QY',
-    'https://www.youtube.com/watch?v=JtG3UAm38iE'
+    'https://www.youtube.com/watch?v=JtG3UAm38iE',
+    'https://www.youtube.com/watch?v=LWBGQJMrut0',
+    'https://www.youtube.com/watch?v=VXE1l_BiSD8'
 ];
