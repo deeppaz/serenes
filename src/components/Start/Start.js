a0c0089e3da9835cdc8839c6a773a54c8592412c
jimmy durak <deeppaz@hotmail.com>
2021-09-20 20:46:15 +0300

    add file to component

---
+++ src/components/Start/Start.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Start.css'
+
+export class Start extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default Start
