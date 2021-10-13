c08bc72e7c08816071841b2d815e8626c74e1ac1
jimmy durak <deeppaz@hotmail.com>
2021-10-13 18:01:22 +0300

    for vercel

---
+++ src/services/config/Api.js
@@ -0,0 +1,11 @@
+const config = {
+    API_KEY: "AIzaSyDJxHGhc8bqjfluv2M2z05WpBD3jpDRs7A",
+    AUTH_DOMAIN: "serenes.firebaseapp.com",
+    PROJECT_ID: "serenes",
+    STORAGE_BUCKET: "serenes.appspot.com",
+    MESSAGING_SENDER_ID: "1057359758485",
+    APP_ID: "1:1057359758485:web:bbb4732753b4359419879c",
+    MEASUREMENT_ID: "G-XFNB99KKNV",
+}
+
+export default config;
