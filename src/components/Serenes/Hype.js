76f270e6c49b2c1fb0026d95d03f46b9ee024639
jimmy durak <deeppaz@hotmail.com>
2021-10-13 16:55:21 +0300

    a clock timer update

--- src/components/Serenes/Hype.js
+++ src/components/Serenes/Hype.js
@@ -7,15 +7,33 @@ import HomePage from "../../assets/image/icons/home.svg";
 import { Link } from "react-router-dom";
 
 const Hype = () => {
+  const color = [
+    { colors: "saat renkler" },
+    { colors: "saat renkler2" },
+    { colors: "saat renkler3" },
+    { colors: "saat renkler4" },
+    { colors: "saat renkler5" },
+    { colors: "saat renkler6" },
+    { colors: "saat renkler7" },
+    { colors: "saat renkler8" },
+  ];
   const [hypegifs, setHypegifs] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [randomCustomColor, setRandomCustomColor] = useState(0);
 
   useEffect(() => {
     const getArrayNumber = Math.floor(Math.random() * data.length);
-    const interval = setInterval(() => {
+    const getColorNumber = Math.floor(Math.random() * color.length);
+    setInterval(() => {
       setHypegifs(getArrayNumber);
+      setRandomCustomColor(color[getColorNumber].colors);
     }, 11000);
-    return () => clearInterval(interval);
+    setInterval(() => tick(), 1000);
   }, [hypegifs]);
+
+  function tick() {
+    setDate(new Date());
+  }
 
   return (
     <div className="playlist-body">
@@ -26,6 +44,8 @@ const Hype = () => {
           <img src={HomePage} alt="home" width="50px" height="50px" />
         </button>
       </Link>
+      <h1 className={randomCustomColor}>{date.toLocaleTimeString()}</h1>
+      {/* <h1 className={anecdotes[randomCustomColor]}>15:41:33</h1> */}
       <img
         src={data[hypegifs].gifs}
         style={{
